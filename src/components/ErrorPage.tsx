debe99ac452fdc887d8c0528326fc499c531a20e
joshuasearle <joshryansearle@gmail.com>
2020-08-14 20:12:38 +1000

    Add error page

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface ErrorPageProps {}
+
+const ErrorPage: React.SFC<ErrorPageProps> = () => {
+  return <h1>ErrorPage</h1>;
+};
+
+export default ErrorPage;
