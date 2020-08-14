979bc00b3af5991727a1fe5ca5394ec4f7fe4cdd
joshuasearle <joshryansearle@gmail.com>
2020-08-14 19:38:54 +1000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/main.css';
+
+ReactDOM.render(<h1 className='title'>Hello, World!</h1>, document.getElementById('root'));
