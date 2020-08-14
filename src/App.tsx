3b7603d7e586e398f1e5cac5ae416801b07e47bf
joshuasearle <joshryansearle@gmail.com>
2020-08-14 20:14:02 +1000

    Clean up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import DayForecast from './components/DayForecast';
 import WeekForecast from './components/WeekForecast';
+import ErrorPage from './components/ErrorPage';
 
 const App: React.SFC = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/day' component={DayForecast} />
-        <Route exact path='/week' comonent={WeekForecast} />
+        <Route exact path='/week' component={WeekForecast} />
+        <Route path='/' component={ErrorPage} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 };
 
