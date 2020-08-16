b194bd7df94c48c0e645935fca63e8c6d2e1d07d
joshuasearle <joshryansearle@gmail.com>
2020-08-16 14:05:27 +1000

    Make mars image an img rather than background image

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,25 @@ import Home from './components/Home';
 import CurrentForecast from './components/Current';
 import WeekForecast from './components/WeekForecast';
 import ErrorPage from './components/ErrorPage';
+import background from './assets/mars.jpg';
+import classes from './css/classes';
 
 const App: React.SFC = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <main>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/current' component={CurrentForecast} />
-          <Route exact path='/week' component={WeekForecast} />
-          <Route path='/' component={ErrorPage} />
-        </Switch>
+        <div className={classes.Image}>
+          <img src={background} alt='Mars the planet' />
+        </div>
+        <div className={classes.Content}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/current' component={CurrentForecast} />
+            <Route exact path='/week' component={WeekForecast} />
+            <Route path='/' component={ErrorPage} />
+          </Switch>
+        </div>
       </main>
     </BrowserRouter>
   );
